28474685cb8b8b3cc9c51a957b7466a9d2bcc4f4
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-01-02 04:19:17 -0500

    First commit

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger } from "@nestjs/common";
+import { AppModule } from './app.module';
+import { envs } from './config/envs';
+
+async function bootstrap() {
+  const logger = new Logger("Orders");
+
+  const app = await NestFactory.create(AppModule);
+  await app.listen(envs.port);
+
+  logger.log(`Orders Microservice running on port ${envs.port}`)
+}
+bootstrap();
