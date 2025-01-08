e897b5109ea028f3323905b8e55bb8b0d00d0998
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-01-08 04:56:17 -0500

    DB created and some endpoints added

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,22 +1,29 @@
 import 'dotenv/config';
-import * as joi from 'joi'; 
+import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  DATABASE_URL: string;
 }
 
-const envsSchema = joi.object({
-  PORT: joi.number().required() 
-}).unknown(true);
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    DATABASE_URL: joi.string().required(),
+  })
+  .unknown(true);
 
-const {error, value} = envsSchema.validate(process.env); 
+const { error, value } = envsSchema.validate(process.env);
 
 if (error) {
-  throw new Error(`Config validation error: ${error}`)
+  throw new Error(`Config validation error: ${error}`);
 }
 
 const envVars: EnvVars = value;
 
 export const envs = {
-  port: envVars.PORT, 
-}
+  port: envVars.PORT,
+  db: {
+    url: envVars.DATABASE_URL,
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger } from "@nestjs/common";
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const logger = new Logger("Orders");
+  const logger = new Logger('Orders');
 
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
 
-  logger.log(`Orders Microservice running on port ${envs.port}`)
+  await app.listen();
+
+  logger.log(`Orders Microservice running on port ${envs.port}`);
 }
 bootstrap();
