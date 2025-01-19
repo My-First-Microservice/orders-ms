ee44e15077828c60768a6ab5db8a80bc3a1e4a4a
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-01-19 10:36:14 -0500

    Get all orders

---
+++ src/orders/dto/order-pagination.dto.ts
@@ -0,0 +1,22 @@
+import { IsEnum, IsOptional, IsPositive } from 'class-validator';
+import { OrderStatusList } from '../enum/order.enum';
+import { Type } from 'class-transformer';
+import { OrderStatus } from '@prisma/client';
+
+export class OrderPaginationDto {
+  @IsPositive()
+  @IsOptional()
+  @Type(() => Number)
+  page?: number = 1;
+
+  @IsPositive()
+  @IsOptional()
+  @Type(() => Number)
+  limit?: number = 10;
+
+  @IsOptional()
+  @IsEnum(OrderStatusList, {
+    message: `Valid status are ${OrderStatusList}`,
+  })
+  status: OrderStatus;
+}
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -2,6 +2,7 @@ import { HttpStatus, Injectable, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { PrismaClient } from '@prisma/client';
 import { RpcException } from '@nestjs/microservices';
+import { OrderPaginationDto } from './dto/order-pagination.dto';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
@@ -15,8 +16,25 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     });
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async findAll(orderPaginationDto: OrderPaginationDto) {
+    const { status, page, limit } = orderPaginationDto;
+
+    const totalPages = await this.order.count({
+      where: { status },
+    });
+
+    return {
+      data: await this.order.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: { status },
+      }),
+      meta: {
+        total: totalPages,
+        page,
+        lastPage: Math.ceil(totalPages / limit),
+      },
+    };
   }
 
   async findOne(id: string) {
